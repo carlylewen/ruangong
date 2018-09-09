dd004bf526be8d14aa1d54c5ace15ac8219caafb
cshao-library <cshao@gdut.edu.cn>
2018-09-09 14:40:13 +0800

    second_commit

--- 第一次作业/src/wcmain.java
+++ 第一次作业/src/wcmain.java
@@ -1,6 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class wcmain {
 
+	public int cword=0;//单词
+	public int cline=1;//行数
+	public int cchar=0;//字符数
+	
+	public void statistical( )
+	{
+		int words=0;//单词
+		int lines=0;//行数
+		int chars=0;//字母
+		int spacing=0;//空格
+		int mark=0;//标点
+		int c=0;//字符
+		int t=0;//'\t'
+		int count=0;//
+		FileInputStream file =null;
+		BufferedReader bf=null;
+		try {
+			
+			
+			
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
